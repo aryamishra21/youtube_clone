f75cdaf22ea32976bf31a9bb0bbc3edbf6747487
aryamishra21 <aryamishra212001@gmail.com>
2025-03-04 12:04:38 +0530

    latest code.

--- src/hooks/useLiveChat.jsx
+++ src/hooks/useLiveChat.jsx
@@ -3,6 +3,69 @@ import { LIVE_STATS, LiveChatURL } from '../utils/constants'
 import { useDispatch, useSelector } from 'react-redux'
 import { addLiveChatData, addNextPageOffset } from '../utils/liveChatSlice'
 
+
+const useLiveChat = () => {
+    const {nextPageOffset, liveChatCurrVideoId} = useSelector((store) => store.liveChat);
+    const dispatch = useDispatch();
+    const [liveChatId, setLiveChatId] = useState(null);
+    const nextPageRef = useRef(nextPageOffset);
+
+    useEffect(() => {
+        nextPageRef.current = nextPageOffset;
+    }, [nextPageOffset]);
+
+    useEffect(() => {
+        if (liveChatCurrVideoId) {
+            getActiveLiveChatId();
+        }
+    }, [liveChatCurrVideoId]);
+
+    const getActiveLiveChatId = async () => {
+        const response = await fetch(LIVE_STATS + '&id=' + liveChatCurrVideoId);
+        const json = await response.json();
+        setLiveChatId(json?.items?.[0]?.liveStreamingDetails?.activeLiveChatId);
+    };
+
+    useEffect(() => {
+        let timer;
+        if (liveChatId) {
+            const fetchData = async () => {
+                try {
+                    const currentPageToken = nextPageRef.current;
+                    const response = await fetch(
+                        LiveChatURL + 
+                        '&liveChatId=' + liveChatId + 
+                        '&pageToken=' + (currentPageToken || '')
+                    );
+                    const json = await response.json();
+                    
+                    if(json.items?.length > 0) {
+                        dispatch(addNextPageOffset(json.nextPageToken));
+                        dispatch(addLiveChatData(json.items));
+                    }
+                } catch (error) {
+                    console.error('Error fetching live chat:', error);
+                }
+            };
+
+            fetchData();
+            
+            timer = setInterval(fetchData, 5000);
+        }
+        return () => clearInterval(timer);
+    }, [liveChatId]); 
+
+    return null;
+};
+export default useLiveChat
+
+
+
+
+
+
+
+
 // const useLiveChat = () => {
 //     const {nextPageOffset,liveChatCurrVideoId} = useSelector((store) => store.liveChat);
 //     const dispatch=useDispatch()
@@ -44,63 +107,3 @@ import { addLiveChatData, addNextPageOffset } from '../utils/liveChatSlice'
 
 // export default useLiveChat
 
-
-
-// In useLiveChat.js
-const useLiveChat = () => {
-    const {nextPageOffset, liveChatCurrVideoId} = useSelector((store) => store.liveChat);
-    const dispatch = useDispatch();
-    const [liveChatId, setLiveChatId] = useState(null);
-    const nextPageRef = useRef(nextPageOffset);
-
-    // Keep ref updated with latest page token
-    useEffect(() => {
-        nextPageRef.current = nextPageOffset;
-    }, [nextPageOffset]);
-
-    useEffect(() => {
-        if (liveChatCurrVideoId) {
-            getActiveLiveChatId();
-        }
-    }, [liveChatCurrVideoId]);
-
-    const getActiveLiveChatId = async () => {
-        const response = await fetch(LIVE_STATS + '&id=' + liveChatCurrVideoId);
-        const json = await response.json();
-        setLiveChatId(json?.items?.[0]?.liveStreamingDetails?.activeLiveChatId);
-    };
-
-    useEffect(() => {
-        let timer;
-        if (liveChatId) {
-            const fetchData = async () => {
-                try {
-                    const currentPageToken = nextPageRef.current;
-                    const response = await fetch(
-                        LiveChatURL + 
-                        '&liveChatId=' + liveChatId + 
-                        '&pageToken=' + (currentPageToken || '')
-                    );
-                    const json = await response.json();
-                    
-                    if(json.items?.length > 0) {
-                        dispatch(addNextPageOffset(json.nextPageToken));
-                        dispatch(addLiveChatData(json.items));
-                    }
-                } catch (error) {
-                    console.error('Error fetching live chat:', error);
-                }
-            };
-
-            // Initial immediate fetch
-            fetchData();
-            
-            // Set up periodic polling
-            timer = setInterval(fetchData, 5000);
-        }
-        return () => clearInterval(timer);
-    }, [liveChatId]); // Only depend on liveChatId
-
-    return null;
-};
-export default useLiveChat
